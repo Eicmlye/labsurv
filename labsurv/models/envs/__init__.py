da1a6984c5a5e13584382e26a67f39302cc780ba
Eric Monlye <536682885@qq.com>
2024-06-21 17:50:41 +0800

    [framework] enable dqn for reconstructed framework

--- labsurv/models/envs/__init__.py
+++ labsurv/models/envs/__init__.py
@@ -1,9 +1,11 @@
 from .base_env import BaseEnv
+from .cart_pole_env import CartPoleEnv
 from .cliff_walk_env import CliffWalkEnv
 from .cliff_walk_model_free_env import CliffWalkModelFreeEnv
 
 __all__ = [
     "BaseEnv",
+    "CartPoleEnv",
     "CliffWalkEnv",
     "CliffWalkModelFreeEnv",
 ]
