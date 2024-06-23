38db0bc1c329fbd775c084e534677fa627fa0f74
Eric Monlye <536682885@qq.com>
2024-06-24 00:06:01 +0800

    [surveil] add Room load, save, and visualize

--- labsurv/models/envs/room/base_room.py
+++ labsurv/models/envs/room/base_room.py
@@ -1,5 +1,6 @@
 import os
 import os.path as osp
+import pickle
 from typing import List
 
 import numpy as np
@@ -9,20 +10,46 @@ from pyntcloud import PyntCloud
 
 
 class BaseRoom:
-    def __init__(self, shape: List[int]):
-        if not (
-            len(shape) == 3
-            and isinstance(shape[0], int)
-            and isinstance(shape[1], int)
-            and isinstance(shape[2], int)
-        ):
-            raise ValueError(
-                "A room should be in 3d shape with all side lengths integers."
-            )
+    def __init__(self, shape: List[int] | None = None, load_from: str | None = None):
+        """
+        Argument:
 
-        self.shape = np.array(shape)
-        self.occupancy = []
-        self.occ_color = []
+            shape (List[int] | None): If is not `None`, a `Room` of `shape` size will
+            be generated.
+
+            load_from (str | None): only valid if `shape` is `None`, and the `Room`
+            will be loaded from `load_from`.
+        """
+
+        assert (
+            shape is not None or load_from is not None
+        ), "At least one of `shape` and `load_from` should be specified."
+
+        if shape is not None:
+            if not (
+                len(shape) == 3
+                and isinstance(shape[0], int)
+                and isinstance(shape[1], int)
+                and isinstance(shape[2], int)
+            ):
+                raise ValueError(
+                    "A room should be in 3d shape with all side lengths integers."
+                )
+
+            self.shape = np.array(shape)
+            self.occupancy = []
+            self.occ_color = []
+        else:
+            if not osp.exists(load_from):
+                raise ValueError(f"The path {load_from} does not exist.")
+            if not load_from.endswith(".pkl"):
+                raise ValueError("Only `pkl` files can be loaded.")
+
+            with open(load_from, "rb") as fpkl:
+                room_data = pickle.load(fpkl)
+                self.shape = room_data["shape"]
+                self.occupancy = room_data["occupancy"]
+                self.occ_color = room_data["occ_color"]
 
     def _check_inside_room(self, points: np.ndarray | List[np.ndarray]):
         for point in points:
@@ -65,6 +92,30 @@ class BaseRoom:
                 self.occ_color.append(point_color)
 
     def save(self, save_path: str):
+        """
+        Saves the `Room` class data in a `pkl` file.
+        """
+        if not save_path.endswith(".pkl"):
+            os.makedirs(save_path, exist_ok=True)
+            save_pkl_path = osp.join(save_path, "BaseRoom.pkl")
+        else:
+            os.makedirs(osp.dirname(save_path), exist_ok=True)
+            save_pkl_path = save_path
+
+        with open(save_pkl_path, "wb") as fpkl:
+            pickle.dump(
+                dict(
+                    shape=self.shape,
+                    occupancy=self.occupancy,
+                    occ_color=self.occ_color,
+                ),
+                fpkl,
+            )
+
+    def visualize(self, save_path: str):
+        """
+        Saves the pointcloud in a `ply` file.
+        """
         show_points_with_color = np.stack(
             (self.occupancy, self.occ_color), axis=1
         ).reshape(-1, 6)
@@ -76,8 +127,9 @@ class BaseRoom:
 
         if not save_path.endswith(".ply"):
             os.makedirs(save_path, exist_ok=True)
-            save_path = osp.join(save_path, "BaseRoom.ply")
+            save_ply_path = osp.join(save_path, "BaseRoom.ply")
         else:
             os.makedirs(osp.dirname(save_path), exist_ok=True)
+            save_ply_path = save_path
 
-        pointcloud.to_file(save_path)
+        pointcloud.to_file(save_ply_path)
