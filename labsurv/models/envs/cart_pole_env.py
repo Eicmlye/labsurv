94f2fe6a7afeaa66b90252b27223b61afb5acbc5
Eric Monlye <536682885@qq.com>
2024-08-22 14:47:33 +0800

    [surveil][dqn] add test mode, cached buffer, checkpoint saving, visualization and customized logging for dqn

--- labsurv/models/envs/cart_pole_env.py
+++ labsurv/models/envs/cart_pole_env.py
@@ -1,6 +1,7 @@
 # CartPoleEnv is reconstructed from gym into mmcv format
-
 import math
+import os
+import os.path as osp
 from functools import partial
 from typing import Any, Dict, Optional
 
@@ -8,6 +9,7 @@ import numpy as np
 from gym import spaces
 from labsurv.builders import ENVIRONMENTS
 from labsurv.models.envs import BaseEnv
+from PIL import Image, ImageDraw, ImageFont
 
 
 @ENVIRONMENTS.register_module()
@@ -113,6 +115,9 @@ class CartPoleEnv(BaseEnv):
         self.observation_space = spaces.Box(-high, high, dtype=np.float32)
         self.init_observ = partial(self._np_random.uniform, size=(4,))
 
+        # visualization
+        self.frames = []
+
     def step(self, observation, action) -> Dict[str, Any]:
         """
         Run one timestep of the environment's dynamics.
@@ -148,7 +153,7 @@ class CartPoleEnv(BaseEnv):
             theta_dot = theta_dot + self.tau * thetaacc
             theta = theta + self.tau * theta_dot
 
-        new_observation = (x, x_dot, theta, theta_dot)
+        new_observation = np.array([x, x_dot, theta, theta_dot], dtype=np.float32)
 
         terminated = bool(
             x < -self.x_threshold
@@ -184,3 +189,124 @@ class CartPoleEnv(BaseEnv):
         init_observation = self.init_observ(low=-0.05, high=0.05)
 
         return np.array(init_observation, dtype=np.float32)
+
+    def render(self, observation: np.ndarray, step_index: int):
+        width, height = 1000, 300  # pixel
+
+        unit = width / (self.x_threshold * 3)
+        pole_length = self.length * unit
+        cart_width, cart_height = pole_length, pole_length / 2
+        center = (width / 2, height / 2)
+
+        img = Image.new("RGB", (width, height), (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        # draw cart
+        draw.rectangle(
+            (
+                center[0] - cart_width / 2 + observation[0] * unit,
+                center[1],
+                center[0] + cart_width / 2 + observation[0] * unit,
+                center[1] + cart_height,
+            ),
+            fill="orange",
+        )
+
+        # draw pole
+        draw.line(
+            (
+                (center[0] + observation[0] * unit, center[1] + cart_height / 2),
+                (
+                    center[0]
+                    + observation[0] * unit
+                    + pole_length * math.sin(observation[2]),
+                    center[1]
+                    + cart_height / 2
+                    - pole_length * math.cos(observation[2]),
+                ),
+            ),
+            width=3,
+            fill=(0, 255, 0),
+        )
+
+        # write step number
+        font_path = "labsurv/utils/string/TIMES.TTF"  # Times New Roman
+        font_size = 30
+        font = ImageFont.truetype(font_path, font_size)
+        draw.text((30, 30), f"Step {step_index}", font=font, fill="black")
+
+        # draw cart limit
+        draw.line(
+            (
+                (center[0] - self.x_threshold * unit, center[1]),
+                (center[0] - self.x_threshold * unit, center[1] + cart_height),
+            ),
+            width=1,
+            fill="red",
+        )
+        draw.line(
+            (
+                (center[0] + self.x_threshold * unit, center[1]),
+                (center[0] + self.x_threshold * unit, center[1] + cart_height),
+            ),
+            width=1,
+            fill="red",
+        )
+
+        # draw pole limit
+        draw.line(
+            (
+                (center[0] + observation[0] * unit, center[1] + cart_height / 2),
+                (
+                    center[0]
+                    + observation[0] * unit
+                    + pole_length * math.sin(self.theta_threshold_radians),
+                    center[1]
+                    + cart_height / 2
+                    - pole_length * math.cos(self.theta_threshold_radians),
+                ),
+            ),
+            width=1,
+            fill="red",
+        )
+        draw.line(
+            (
+                (center[0] + observation[0] * unit, center[1] + cart_height / 2),
+                (
+                    center[0]
+                    + observation[0] * unit
+                    + pole_length * math.sin(-self.theta_threshold_radians),
+                    center[1]
+                    + cart_height / 2
+                    - pole_length * math.cos(-self.theta_threshold_radians),
+                ),
+            ),
+            width=1,
+            fill="red",
+        )
+
+        # display live
+        # img.show()
+
+        self.frames.append(img)
+
+    def save_gif(self, gif_path: str = "cart_pole.gif"):
+        if len(self.frames) == 0:
+            raise ValueError("No frames appended yet.")
+
+        if gif_path.endswith(".gif"):
+            os.makedirs(osp.dirname(gif_path), exist_ok=True)
+        else:
+            os.makedirs(gif_path, exist_ok=True)
+            gif_path = osp.join(gif_path, "cart_pole.gif")
+
+        self.frames[0].save(
+            gif_path,
+            save_all=True,
+            append_images=self.frames[1:],
+            loop=0,  # loop time, 0 for forever
+            duration=100,  # duration for every frame
+        )
+
+        # clear cached frames
+        self.frames = []
--- labsurv/runners/step_based_runner.py
+++ labsurv/runners/step_based_runner.py
@@ -1,3 +1,5 @@
+import os.path as osp
+
 from labsurv.builders import AGENTS, ENVIRONMENTS, HOOKS, REPLAY_BUFFERS
 from mmcv import Config
 from mmcv.utils import ProgressBar
@@ -5,25 +7,81 @@ from mmcv.utils import ProgressBar
 
 class StepBasedRunner:
     def __init__(self, cfg: Config):
+        self.work_dir = cfg.work_dir
+        self.logger = HOOKS.build(cfg.logger_cfg)
+
         self.env = ENVIRONMENTS.build(cfg.env)
         self.agent = AGENTS.build(cfg.agent)
-        self.logger = HOOKS.build(cfg.logger_cfg)
-        self.replay_buffer = (
-            REPLAY_BUFFERS.build(cfg.replay_buffer) if cfg.use_replay_buffer else None
-        )
+        self.test_mode = self.agent.test_mode
+        self.start_episode = 0
 
-        # max episode number
-        self.episodes = cfg.episodes
         # max step number for an episode, exceeding makes truncated True
         self.steps = cfg.steps
 
-    def run(self):
+        if not self.test_mode:
+            self.replay_buffer = None
+            if cfg.use_replay_buffer:
+                self.replay_buffer = REPLAY_BUFFERS.build(cfg.replay_buffer)
+                if "load_from" not in cfg.replay_buffer.keys():
+                    self.generate_replay_buffer()
+
+            self.save_checkpoint_interval = cfg.save_checkpoint_interval
+
+            # max episode number
+            self.episodes = cfg.episodes
+
+            self.start_episode = self.agent.start_episode
+            if "resume_from" in cfg.agent.keys() and cfg.agent.resume_from is not None:
+                self.logger.set_cur_episode_index(self.start_episode)
+
+    def generate_replay_buffer(self):
         cur_observation = None
-        for episode in range(self.episodes):
+
+        # generate replay buffer
+        print("Replay buffer generating...")
+        if self.replay_buffer is not None:
+            prog_bar = ProgressBar(self.replay_buffer.activate_size)
+            while not self.replay_buffer.is_active():
+                cur_observation = self.env.reset()
+                terminated = False
+
+                for step in range(self.steps):
+                    if terminated or self.replay_buffer.is_active():
+                        break
+
+                    cur_action = self.agent.take_action(cur_observation)
+
+                    transition = self.env.step(cur_observation, cur_action)
+                    transition["cur_observation"] = cur_observation
+                    transition["cur_action"] = cur_action
+
+                    terminated = transition["terminated"]
+
+                    if self.replay_buffer is not None:
+                        self.replay_buffer.add(transition)
+                        prog_bar.update()
+
+                    cur_observation = transition["next_observation"]
+        print("\nReplay buffer generated.")
+
+        # save buffer cache
+        self.replay_buffer.save(self.work_dir)
+
+    def run(self):
+        if self.test_mode:
+            return self.test()
+        else:
+            return self.train()
+
+    def train(self):
+        cur_observation = None
+
+        for episode in range(self.start_episode, self.episodes):
             cur_observation = self.env.reset()
 
             episode_return = 0
             terminated = False
+            loss = None
 
             for step in range(self.steps):
                 if terminated:
@@ -36,6 +94,7 @@ class StepBasedRunner:
                 transition["cur_action"] = cur_action
 
                 terminated = transition["terminated"]
+                # truncated = step == self.steps - 1
 
                 if self.replay_buffer is not None:
                     self.replay_buffer.add(transition)
@@ -46,9 +105,51 @@ class StepBasedRunner:
                 if self.replay_buffer is not None:
                     if self.replay_buffer.is_active():
                         samples = self.replay_buffer.sample()
-                        self.agent.update(samples)
+                        loss = self.agent.update(samples)
                 else:
                     raise NotImplementedError()
 
+            log_dict = dict(lr=self.agent.lr)
+            if loss is not None:
+                log_dict["loss"] = loss
+
             self.logger.update(episode_return)
-            self.logger(self.episodes)
+            self.logger(self.episodes, **log_dict)
+
+            if (episode + 1) % self.save_checkpoint_interval == 0:
+                self.agent.save(episode, self.work_dir)
+                self.replay_buffer.save(
+                    osp.join(self.work_dir, f"episode_{episode + 1}.pkl")
+                )
+
+    def test(self):
+        cur_observation = self.env.reset()
+
+        episode_return = []
+        terminated = False
+
+        print("Rendering test results...")
+        prog_bar = ProgressBar(self.steps)
+        for step in range(self.steps):
+            self.env.render(cur_observation, step)
+
+            if terminated:
+                break
+
+            cur_action = self.agent.take_action(cur_observation)
+
+            transition = self.env.step(cur_observation, cur_action)
+            transition["cur_observation"] = cur_observation
+            transition["cur_action"] = cur_action
+
+            terminated = transition["terminated"]
+
+            cur_observation = transition["next_observation"]
+            episode_return.append(transition["reward"])
+
+            prog_bar.update()
+
+        print("\nSaving gif...")
+        save_path = osp.join(self.work_dir, f"done_step_{prog_bar.completed}.gif")
+        self.env.save_gif(save_path)
+        print(f"GIF saved to {save_path}")
