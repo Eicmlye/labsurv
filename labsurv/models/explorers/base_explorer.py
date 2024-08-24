8cea07d6e9549dd708ef8300b960e6478f621740
Eric Monlye <536682885@qq.com>
2024-08-24 14:55:12 +0800

    [surveil][explorer] made epsilon-greedy explorer a real plugin

--- labsurv/models/explorers/base_explorer.py
+++ labsurv/models/explorers/base_explorer.py
@@ -1,15 +1,24 @@
+from typing import Optional
+
 from labsurv.builders import EXPLORERS
 from labsurv.utils.random import np_random
 
 
 @EXPLORERS.register_module()
 class BaseExplorer:
-    def __init__(self, seed: int | None = None):
+    def __init__(self, seed: Optional[int] = None):
         self._random, _ = np_random(seed)
 
     def decide(self):
         """
         Decide either to explore or to exploit.
+        """
+
+        raise NotImplementedError
+
+    def act(self):
+        """
+        Actions when exploring.
         """
 
         raise NotImplementedError
