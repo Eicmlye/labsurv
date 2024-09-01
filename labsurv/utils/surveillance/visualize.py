fa710537ac61d87d056c67d7e2c47d9dbe0c8d05
Eric Monlye <536682885@qq.com>
2024-09-02 00:09:25 +0800

    [ocp][physics] rewrite SurveillanceRoom with torch and added samples

--- labsurv/utils/surveillance/visualize.py
+++ labsurv/utils/surveillance/visualize.py
@@ -1,49 +1,65 @@
 import os
 import os.path as osp
 from copy import deepcopy
-from typing import List
+from typing import Optional
 
 import numpy as np
 import pandas as pd
+import torch
 from labsurv.utils.surveillance import shift
 from pyntcloud import PyntCloud
+from torch import Tensor
 
 
 def concat_points_with_color(
-    points: np.ndarray | List[np.ndarray],
-    color: np.ndarray,
-    displacement: np.ndarray = np.array([0, 0, 0]),
-):
-    if len(points) == 0:
-        return None
+    points: Tensor, color: np.ndarray, displacement: Optional[Tensor] = None
+) -> Optional[Tensor]:
+    """
+    # Arguments:
 
-    CENTER_SHIFT = np.array([0.5, 0.5, 0.5])
-    result_points = deepcopy(points)
-    if len(result_points[0]) > 3:
-        result_points = [result_point[:3] for result_point in result_points]
-    elif len(result_points[0]) < 3:
+        `points` (Tensor): Width * Depth * Height tensor
+    """
+    device = points.device
+    if displacement is None:
+        displacement = torch.tensor([0, 0, 0], dtype=torch.float16, device=device)
+    elif displacement.device != device:
+        displacement = displacement.to(device)
+    CENTER_SHIFT = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float16, device=device)
+
+    if points.ndim > 3:
+        points = points[:, :, :, 0]
+    elif points.ndim < 3:
         raise ValueError("No valid 3D coordinates found in given point list.")
 
-    return np.stack(
-        (
-            shift(result_points, displacement + CENTER_SHIFT),
-            np.array([color] * len(result_points)),
-        ),
-        axis=1,
-    ).reshape(-1, 6)
+    points = points.nonzero()
+    if points.numel() == 0:
+        return None
+
+    points = shift(points, displacement + CENTER_SHIFT)
+    colors = torch.tensor(color, dtype=torch.float16, device=device).repeat(
+        len(points), 1
+    )
+
+    return torch.cat((points, colors), 1)
 
 
 def save_visualized_points(
-    points_with_color: np.ndarray, save_path: str, default_filename: str = "pointcloud"
+    points_with_color: Tensor, save_path: str, default_filename: str = "pointcloud"
 ):
     """
     Save input points_with_color to `ply` file.
     """
+    # NOTE(eric): VSCode pointcloud visualization cannot render `torch.float16` points,
+    # and the visualization will be fully green screen. Changing to `torch.float` is
+    # necessary.
+    points_with_color = points_with_color.cpu().type(torch.float)
+
     framed_points_with_color = pd.DataFrame(points_with_color)
     framed_points_with_color.columns = ["x", "y", "z", "red", "green", "blue"]
     pointcloud = PyntCloud(framed_points_with_color)
 
     pointcloud.add_scalar_field("rgb_intensity")
+    # print(pointcloud)
 
     if not save_path.endswith(".ply"):
         os.makedirs(save_path, exist_ok=True)
