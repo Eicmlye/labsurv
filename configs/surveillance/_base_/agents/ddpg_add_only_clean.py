48a6a67df30f0b6f05d426cc889c8a3e2edac4fe
Eric Monlye <536682885@qq.com>
2025-01-15 14:38:54 +0800

    [ocp][ddpg] made conv layer smaller and decreased lr

--- configs/surveillance/_base_/agents/ddpg_add_only_clean.py
+++ configs/surveillance/_base_/agents/ddpg_add_only_clean.py
@@ -13,7 +13,7 @@ agent_cfg = dict(
         actor_cfg=dict(
             type="OCPDDPGAddOnlyCleanPolicyNet",
             device=DEVICE,
-            hidden_dim=64,
+            hidden_dim=32,
             action_dim=action_num,  # add # , del, adjust, stop
             cam_types=1,
             neck_layers=1,
@@ -23,7 +23,7 @@ agent_cfg = dict(
         critic_cfg=dict(
             type="OCPDDPGCleanValueNet",
             device=DEVICE,
-            neck_hidden_dim=64,
+            neck_hidden_dim=32,
             adaptive_pooling_dim=10,
             neck_layers=1,
         ),
@@ -42,9 +42,9 @@ agent_cfg = dict(
         ),
         device=DEVICE,
         gamma=0.98,
-        actor_lr=1e-4,
-        critic_lr=1e-2,
-        tau=2e-1,
+        actor_lr=1e-5,
+        critic_lr=1e-4,
+        tau=1e-1,
         # load_from=None,
         # resume_from="output/ocp/ddpg/trail/episode_5.pth",
     ),
