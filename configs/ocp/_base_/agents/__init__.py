6f0f8d7211ff7e2af52e3dfccccf3b1610db741a
Eric Monlye <536682885@qq.com>
2025-03-04 22:47:59 +0800

    [ocp][benchmark] added benchmark configs and param generator

--- configs/ocp/_base_/agents/__init__.py
+++ configs/ocp/_base_/agents/__init__.py
@@ -1,3 +1,4 @@
+from .mappo_benchmark import agent_cfg as mappo_benchmark_agent
 from .mappo_pointnet2 import agent_cfg as mappo_pointnet2_agent
 from .multi_agent_ppo import agent_cfg as multi_agent_ppo_agent
 from .reinforce import agent_cfg as reinforce_agent
@@ -6,4 +7,5 @@ __all__ = [
     "reinforce_agent",
     "multi_agent_ppo_agent",
     "mappo_pointnet2_agent",
+    "mappo_benchmark_agent"
 ]
