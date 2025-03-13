0a71c13bb9e2e6435ec62b16ac31330caac6b7d8
Eric Monlye <536682885@qq.com>
2025-03-14 01:26:18 +0800

    [ocp][infra] fixed bug on circular import

--- labsurv/models/buffers/__init__.py
+++ labsurv/models/buffers/__init__.py
@@ -1,7 +1,12 @@
 from .base_replay_buffer import BaseReplayBuffer
+
+# isort: off
+# base class must be imported before children
 from .cliff_walk_prioritized_replay_buffer import CliffWalkPrioritizedReplayBuffer
 from .ocp_prioritized_replay_buffer import OCPPrioritizedReplayBuffer
 from .ocp_replay_buffer import OCPReplayBuffer
+
+# isort: on
 
 __all__ = [
     "BaseReplayBuffer",
--- labsurv/models/explorers/__init__.py
+++ labsurv/models/explorers/__init__.py
@@ -1,7 +1,17 @@
-from .base_epsilon_greedy_explorer import BaseEpsilonGreedyExplorer
 from .base_explorer import BaseExplorer
+
+# isort: off
+# base class must be imported before children
+from .base_epsilon_greedy_explorer import BaseEpsilonGreedyExplorer
+
+# isort: on
+
+# isort: off
+# base class must be imported before children
 from .ocp_epsilon_greedy_explorer import OCPEpsilonGreedyExplorer
 from .ocp_rnd_explorer import OCPRNDExplorer
+
+# isort: on
 
 __all__ = [
     "BaseEpsilonGreedyExplorer",
