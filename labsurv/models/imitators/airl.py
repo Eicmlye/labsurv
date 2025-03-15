031f5c2fd629d2a94a77099bc98d94c0aa5a1565
Eric Monlye <536682885@qq.com>
2025-03-15 21:34:16 +0800

    [ocp][expert] fixed bug on AIRL

--- labsurv/models/imitators/airl.py
+++ labsurv/models/imitators/airl.py
@@ -318,8 +318,9 @@ class AIRL(BaseImitator):
             discriminator_loss.backward()
 
             rewards_list += (
-                torch.log(agent_disc_prob) - torch.log(1 - agent_disc_prob)
+                torch.log(1 - agent_disc_prob) - torch.log(agent_disc_prob)
             ).tolist()
+
             agent_probs += agent_disc_prob.view(-1).tolist()
             expert_probs += expert_disc_prob.view(-1).tolist()
 
