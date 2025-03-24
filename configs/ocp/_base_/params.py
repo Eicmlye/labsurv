82f3bc7fc7e545231b0226a84035969af9b40c43
Eric Monlye <536682885@qq.com>
2025-03-24 22:25:56 +0800

    [ocp][config] update config

--- configs/ocp/_base_/params.py
+++ configs/ocp/_base_/params.py
@@ -1,18 +1,33 @@
-from configs.ocp._base_.std_surveil_test import cam_intrinsics as CAM_TYPES
-from configs.ocp._base_.std_surveil_test import voxel_length
+from configs.ocp._base_.std_surveil_standard import cam_intrinsics as CAM_TYPES_STANDARD
+from configs.ocp._base_.std_surveil_standard import (
+    voxel_length as VOXEL_LENGTH_STANDARD,
+)
+from configs.ocp._base_.std_surveil_test import cam_intrinsics as CAM_TYPES_TEST
+from configs.ocp._base_.std_surveil_test import voxel_length as VOXEL_LENGTH_TEST
+from configs.ocp._base_.std_surveil_tiny import cam_intrinsics as CAM_TYPES_TINY
+from configs.ocp._base_.std_surveil_tiny import voxel_length as VOXEL_LENGTH_TINY
 from numpy import pi as PI
 
-ROOM_NAME = "surv_room_test"
+ROOM_NAME = "surv_room_tiny"
+room_id = ROOM_NAME[10:]
 
-AGENT_NUM = 10  # should be greater than 1
-CAM_TYPE_NUM = len(CAM_TYPES)
+AGENT_NUM = 4  # should be greater than 1
+CAM_TYPE_NUM = len(
+    CAM_TYPES_TEST
+    if room_id == "test"
+    else (CAM_TYPES_STANDARD if room_id == "standard" else CAM_TYPES_TINY)
+)
 PAN_SEC_NUM = 12
 TILT_SEC_NUM = 9
 PAN_RANGE = [-PI, PI]
 TILT_RANGE = [-PI / 2, 0]
-VOXEL_LENGTH = voxel_length
+VOXEL_LENGTH = (
+    VOXEL_LENGTH_TEST
+    if room_id == "test"
+    else (VOXEL_LENGTH_STANDARD if room_id == "standard" else VOXEL_LENGTH_TINY)
+)
 ALLOW_POLAR = True
 MANUAL = False
 BACKBONE_PATH = "labsurv/checkpoint/pointnet2backbone_melt.pth"
-FREEZE_BACKBONE = []
+FREEZE_BACKBONE = [0]
 BACKBONE_RADIUS = 0.1
