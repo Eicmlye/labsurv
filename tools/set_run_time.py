57558e5205e77b2a34f18561e6ecfb798962e5ec
Eric Monlye <536682885@qq.com>
2025-03-29 15:48:16 +0800

    [ocp][log] added log for set_run_time.py

--- tools/set_run_time.py
+++ tools/set_run_time.py
@@ -21,8 +21,11 @@ def parse_args():
     parser.add_argument(  # --command, -c
         "--command", "-c", type=str, default=None, help="The command to run."
     )
-    parser.add_argument(
+    parser.add_argument(  # --sleep-step, -s
         "--sleep-step", "-s", type=int, default=300, help="Check time step in seconds."
+    )
+    parser.add_argument(  # --log, -l
+        "--log", "-l", type=str, default="output/shell/", help="Log directory."
     )
 
     args = parser.parse_args()
@@ -64,12 +67,38 @@ def check_time_stamp_after(cur_time_stamp: str, target_time_stamp: str):
 
 def main():
     args = parse_args()
+    log_time = get_time_stamp()
+    log_filename = (
+        "_".join(["".join(log_time.split("-")[:3]), "".join(log_time.split("-")[3:])])
+        + ".log"
+    )
+    log_path = os.path.join(args.log, log_filename)
+    print(f"Log will be saved at {log_path}.")
+
+    start_prompt = f"{"Start time":<12}{log_time}\n{"Run after":<12}{args.time}"
+    print(start_prompt)
+    if args.log is not None:
+        os.makedirs(args.log, exist_ok=True)
+        with open(log_path, "a") as f:
+            f.write(start_prompt + "\n")
 
     while not check_time_stamp_after(get_time_stamp(), args.time):
-        print(f"Checked at {get_time_stamp()}, not activated.")
+        check_prompt = f"{"Checked at":<12}{get_time_stamp()}, not activated."
+        print("\r\033[K" + check_prompt, end="")
+
+        if args.log is not None:
+            with open(log_path, "a") as f:
+                f.write(check_prompt + "\n")
+
         time.sleep(args.sleep_step)
 
-    print(f"Activated at {get_time_stamp()}")
+    run_prompt = f"{"Activated":<12}{get_time_stamp()}"
+    print("\r\033[K" + run_prompt)
+    if args.log is not None:
+        with open(log_path, "a") as f:
+            f.write(run_prompt + "\n")
+            f.write(f"Will run command \"{args.command}\"")
+
     os.system(args.command)
 
 
